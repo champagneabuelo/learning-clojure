7245d7d5677f190bf11447846d36dba78829b6ce
Jason Valenzuela <jev4zs@virginia.edu>
2017-05-18 18:22:56 -0400

    adding another clj file

--- src/learning_clojure/core.clj
+++ src/learning_clojure/core.clj
@@ -1,55 +1,11 @@
 (ns learning-clojure.core
+  (require [learning-clojure.hobbit :refer :all])
   (:gen-class))
 
-(def asym-hobbit-body-parts [{:name "head" :size 3}
-                             {:name "left-eye" :size 1}
-                             {:name "left-ear" :size 1}
-                             {:name "mouth" :size 1}
-                             {:name "nose" :size 1}
-                             {:name "neck" :size 2}
-                             {:name "left-shoulder" :size 3}
-                             {:name "left-upper-arm" :size 3}
-                             {:name "chest" :size 10}
-                             {:name "back" :size 10}
-                             {:name "left-forearm" :size 3}
-                             {:name "abdomen" :size 6}
-                             {:name "left-kidney" :size 1}
-                             {:name "left-hand" :size 2}
-                             {:name "left-knee" :size 2}
-                             {:name "left-thigh" :size 4}
-                             {:name "left-lower-leg" :size 3}
-                             {:name "left-achilles" :size 1}
-                             {:name "left-foot" :size 2}]
-)
-
-(defn matching-part
-  "helper function for symmetrize-body-parts"
-  [part]
-  {:name (clojure.string/replace (:name part) #"^left-" "right-")
-   :size (:size part)}
-)
-
-(defn symmetrize-body-parts
-  "expects a sequence of maps that have a :name and :size"
-  [asym-body-parts]
-  (loop [remaining-asym-parts asym-body-parts
-         final-body-parts []]
-    (if (empty? remaining-asym-parts)
-      final-body-parts
-      (let [[part & remaining] remaining-asym-parts]
-        (recur remaining
-               (into final-body-parts
-                     (set [part (matching-part part)])
-               )
-        )
-      )
-    )
-  )
-)
 
 (defn -main
   "prints out the output from symmetrize-body-parts"
   [& args] 
-  (println (symmetrize-body-parts asym-hobbit-body-parts))
+  (println "hobbit part hit: " (hit asym-hobbit-body-parts))
 )
 
